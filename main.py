e4347a96cada1acc4ea14871e014cdd57887c745
ArtsemDev <thommy.python.dev@gmail.com>
2023-10-30 21:09:07 +0300

    add is_palindrome function

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ print(len(a))
 print("another print")
 name = input("Enter name: ")
 print(f"{name=}")
+
+
+def is_palindrome(text: str) -> bool:
+    """Проверка текста на палиндром
+
+    :param text: Текст для проверки
+    :return: True - если текст является палиндромом, False - в противном случае
+    """
+    return text.lower() == text.lower()[::-1]
